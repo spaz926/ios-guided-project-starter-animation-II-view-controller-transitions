55e5290c54760d50dbf53e230c307d0cfdee7715
Jeffrey Carpenter <spaz926@me.com>
2019-05-16 12:52:18 -0500

    Added a pan gesture transition

--- Part 2 - Transitions/Transitions/RedViewController.swift
+++ Part 2 - Transitions/Transitions/RedViewController.swift
@@ -13,6 +13,7 @@ class RedViewController: LabelProvidingVC, UIViewControllerTransitioningDelegate
     @IBOutlet weak var label: UILabel!
     
     let animator = Animator()
+    private var interactionController: UIPercentDrivenInteractiveTransition?
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -22,7 +23,29 @@ class RedViewController: LabelProvidingVC, UIViewControllerTransitioningDelegate
         return animator
     }
     
+    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactionController
+    }
+    
     @IBAction func pan(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            interactionController = UIPercentDrivenInteractiveTransition()
+            dismiss(sender)
+        case .changed:
+            let translation = sender.translation(in: view)
+            let percentageComplete = abs(translation.y / view.bounds.height)
+            interactionController?.update(percentageComplete)
+        case .ended:
+            let velocity = sender.velocity(in: view).y
+            if velocity > 100 {
+                interactionController?.finish()
+            } else {
+                interactionController?.cancel()
+            }
+        default:
+            break
+        }
     }
     
     @IBAction func dismiss(_ sender: Any) {
